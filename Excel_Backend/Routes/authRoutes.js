ea59d259c56e82b2b22fe6bf48d74cc18cb75636
Deepthi <deepthisowmyasrirameswarapu@gmail.com>
2025-06-05 12:47:16 +0530

    Project

--- Excel_Backend/Routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerUser, loginUser } = require("../Controller/authController");
-
-// Use the imported functions directly
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-
-module.exports = router;
--- excel_frontend/src/pages/UserDashboard.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function UserDashboard() {
-  return <h1><center>Welcome to the User Dashboard</center></h1>;
-}
-
-export default UserDashboard;
